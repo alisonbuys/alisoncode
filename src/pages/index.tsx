ac8612690c63ea1aa8a8707109e731ac1d83b653
alisonbuys <alisonbuys15@gmail.com>
2023-05-08 12:05:46 +0200

    10

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
         </div>
         <div className="p-5"></div>
         
-        <div className="rounded-2xl bg-slate-100 shadow-inner p-20">
+        <div className="rounded-2xl bg-slate-100 shadow-inner p-10">
         <Image
             height={200}
             width={200}
