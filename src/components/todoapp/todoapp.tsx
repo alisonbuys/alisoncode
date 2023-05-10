131bc2eb43112e474e24d67487ef3ce4b58071ac
Alison Buys <alisonbuys15@gmail.com>
2023-05-10 15:34:44 +0200

    TODO

---
+++ src/components/todoapp/todoapp.tsx
@@ -0,0 +1,142 @@
+// import React,{FormEvent,FunctionCompont as FC} from "react";
+import { useState } from "react";
+import { number } from "zod";
+import {
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
+
+type TodoItem = {
+  id: string;
+  time: Date;
+  title: string;
+  isDone: boolean;
+  order: number;
+};
+
+export function TodoApp() {
+  const [todo, todo_set] = useState<TodoItem[]>([]);
+
+  const [newtodo, newtodo_set] = useState("");
+
+  return (
+    <div className="rounded-xl bg-rose-200 p-8 shadow-2xl">
+      <div className="p-2 text-lg font-normal text-white"> TODO LIST</div>
+      <div>
+        <input
+          type="text"
+          placeholder="Add Todo..."
+          className="mb-5 border p-4"
+          value={newtodo}
+          onChange={(e) => {
+            newtodo_set(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            const newtodoitem: TodoItem = {
+              id: crypto.randomUUID(),
+              title: newtodo,
+              isDone: false,
+              time: new Date(),
+              order: todo.length + 1,
+            };
+
+            todo_set([...todo, newtodoitem]);
+          }}
+          className="rounded-r-xl bg-rose-400 p-4 px-3 text-white transition hover:bg-rose-500"
+        >
+          Add task
+        </button>
+      </div>
+      {todo
+        .sort((a, b) => a.order - b.order)
+        .map((onetodoitem) => {
+          return (
+            <div
+              className={
+                onetodoitem.isDone === true
+                  ? "flex bg-rose-200 p-2 text-gray-500"
+                  : "flex bg-rose-200 p-2 font-semibold"
+              }
+            >
+              <span className="flex-1">
+                {onetodoitem.order} . {onetodoitem.title}
+              </span>
+              <button
+                onClick={() => {
+                  let todolist = todo.map((i) => {
+                    if (i.id === onetodoitem.id) {
+                      i.order = i.order - 1;
+                    }
+
+                    return i;
+                  });
+
+                  todo_set(todolist);
+                }}
+                className="rounded-md border bg-rose-400 p-2 text-white transition hover:bg-rose-500"
+              >
+                {" "}
+                <ChevronUpIcon className="h-5 w-5"></ChevronUpIcon>{" "}
+              </button>
+              <button
+                onClick={() => {
+                  let todolist = todo.map((i) => {
+                    if (i.id === onetodoitem.id) {
+                      i.order = i.order + 1;
+                    }
+
+                    return i;
+                  });
+
+                  todo_set(todolist);
+                }}
+                className="rounded-md border bg-rose-400 p-2 text-white transition hover:bg-rose-500"
+              >
+                {" "}
+                <ChevronDownIcon className="h-5 w-5"></ChevronDownIcon>{" "}
+              </button>
+              <button
+                onClick={() => {
+                  let todolist = todo.map((i) => {
+                    if (i.id === onetodoitem.id) {
+                      i.isDone = true;
+                    }
+
+                    return i;
+                  });
+
+                  todo_set(todolist);
+                }}
+                className="rounded-md border bg-rose-400 p-2 text-white transition hover:bg-rose-500"
+              >
+                {" "}
+                <CheckIcon className="h-5 w-5"></CheckIcon>{" "}
+              </button>
+
+              <button
+                onClick={() => {
+                  let todolist = todo.filter((i) => {
+                    if (i.id === onetodoitem.id) {
+                      return false;
+                    }
+
+                    return true;
+                  });
+
+                  todo_set(todolist);
+                }}
+                className="rounded-md border bg-rose-400 p-2 text-white transition hover:bg-rose-500"
+              >
+                {" "}
+                <XMarkIcon className="h-5 w-5"></XMarkIcon>{" "}
+              </button>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,6 @@ import Link from "next/link";
 import Image from "next/image";
 import { api } from "~/utils/api";
 
-
-
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
@@ -17,21 +15,20 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="p-7">
-        <div className="flex p-4 h-20 bg-red-700">
+        <div className="flex h-20 bg-red-700 p-4">
           <Image
             height={100}
             width={100}
             src={"/assets/images/photo.jpg"}
             className="h-10 w-10 rounded-full "
             alt={"people"}
-          /><div className=" flex-auto mt-2 text-base font-bold">MY PROFILE
-            
-          </div>
+          />
+          <div className=" mt-2 flex-auto text-base font-bold">MY PROFILE</div>
         </div>
         <div className="p-5"></div>
-        
-        <div className="rounded-2xl bg-slate-100 shadow-inner p-10">
-        <Image
+
+        <div className="rounded-2xl bg-slate-100 p-10 shadow-inner">
+          <Image
             height={200}
             width={200}
             src={"/assets/images/photo.jpg"}
@@ -43,8 +40,9 @@ const Home: NextPage = () => {
           <div>
             Outgoing and ambitious high school graduate. Eager to use my
             programming experience and communication skills to work as an
-            software developer. Problem solver eager to prove my value and talent. Motivated to advance and expand my skill
-            set through targeted mentorship and challenging projects.
+            software developer. Problem solver eager to prove my value and
+            talent. Motivated to advance and expand my skill set through
+            targeted mentorship and challenging projects.
           </div>
         </div>
 
@@ -62,7 +60,6 @@ const Home: NextPage = () => {
               <div>
                 <h3 className="border-l border-indigo-600 pl-6 font-semibold text-gray-900">
                   Cell
-                
                 </h3>
                 <address className="border-l border-gray-200 pl-6 pt-2 not-italic text-gray-600">
                   <p>062 340 2856</p>
@@ -81,7 +78,12 @@ const Home: NextPage = () => {
                   Linked in
                 </h3>
                 <address className="border-l border-gray-200 pl-6 pt-2 not-italic text-gray-600">
-                  <Link href={"https://www.linkedin.com/in/alison-buys-a84474275"} className="text-blue-500">https://www.linkedin.com/in/alison-buys-a84474275</Link>
+                  <Link
+                    href={"https://www.linkedin.com/in/alison-buys-a84474275"}
+                    className="text-blue-500"
+                  >
+                    https://www.linkedin.com/in/alison-buys-a84474275
+                  </Link>
                 </address>
               </div>
               <div>
@@ -89,7 +91,12 @@ const Home: NextPage = () => {
                   GitHub
                 </h3>
                 <address className="border-l border-gray-200 pl-6 pt-2 not-italic text-gray-600">
-                <Link href={"https://github.com/alisonbuys"} className="text-blue-500">https://github.com/alisonbuys</Link>
+                  <Link
+                    href={"https://github.com/alisonbuys"}
+                    className="text-blue-500"
+                  >
+                    https://github.com/alisonbuys
+                  </Link>
                 </address>
               </div>
               <div>
@@ -101,16 +108,59 @@ const Home: NextPage = () => {
                   <p>Mossel Bay</p>
                 </address>
               </div>
-             
             </div>
           </div>
         </div>
+        <div className="text-3xl font-bold tracking-tight text-gray-900">
+          Projects{" "}
+        </div>
+        <div className="container mx-auto bg-gray-100 p-4">
+          <h1>Projects</h1>
+          <div className="grid grid-cols-2 grid-rows-4 gap-4">
+            <ProjectThumbnailLinks
+              link={"/projects/messageapp"}
+              name={"Message App"}
+              description={"A simple whatsapp clone built with react."}
+            />
 
-<Link href={"message.tsx"} className="text-blue-300">message</Link>
-        
+            <ProjectThumbnailLinks
+              link={"/projects/todoapp"}
+              name={"TODO App"}
+              description={"A simple todo clone built with react."}
+            />
+
+            {/* <ProjectThumbnailLinks
+              link={"/projects/messageapp"}
+              name={"Message App"}
+              description={"A simple whatsapp clone built with react."}
+            />
+
+            <ProjectThumbnailLinks
+              link={"/projects/messageapp"}
+              name={"Message App"}
+              description={"A simple whatsapp clone built with react."}
+            /> */}
+          </div>
+        </div>
       </main>
     </>
   );
 };
+
+function ProjectThumbnailLinks(props: {
+  link: string;
+  name: string;
+  description: string;
+}) {
+  return (
+    <Link href={props.link} className="">
+      <div className="h-full rounded-lg bg-gray-300 p-4 transition hover:bg-sky-500">
+        <h3 className="text-blue-700">{props.name}</h3>
+
+        <p>{props.description}</p>
+      </div>
+    </Link>
+  );
+}
 
 export default Home;
