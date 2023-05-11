aa2e73772c9d8b223df205d9032fae30344b6eac
Alison Buys <alisonbuys15@gmail.com>
2023-05-11 12:57:46 +0200

    test

---
+++ src/pages/tests.tsx
@@ -0,0 +1,10 @@
+export default function TestPage() {
+  return (
+    <div>
+      TESTS
+      <div className="bg-green-500 sm:bg-blue-500 md:bg-yellow-500 lg:bg-orange-500 xl:bg-purple-500">
+        TEST
+      </div>
+    </div>
+  );
+}
