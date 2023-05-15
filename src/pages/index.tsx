c1f637d76171e186ae8dba7f9b434dfa55870b21
Alison Buys <alisonbuys15@gmail.com>
2023-05-15 09:02:03 +0200

    threed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -132,7 +132,7 @@ const Home: NextPage = () => {
 
             <ProjectThumbnailLinks
               link={"/projects/threed"}
-              name={"React App"}
+              name={"ThreeD App"}
               description={"A simple 3D clone built with react."}
             />
             {/*
