1adde1f7036495e7e069c8f1083c19932bfe22be
Alison Buys <alisonbuys15@gmail.com>
2023-06-01 09:10:24 +0200

    final

--- src/components/pyramidapp/pyramid.tsx
+++ src/components/pyramidapp/pyramid.tsx
@@ -22,49 +22,28 @@ export default function PyramidApp() {
   const [rows, set_rows] = useState<number>(10);
   const steps = generatePyramid(rows);
 
-
-
-  // let singleNumber = 1;
-
-  // for (let rowNumber = 1; (rowNumber = row); rowNumber = rowNumber + 1) {
-  //   console.log(`rowNumber is ${rowNumber}`);
-
-  //   let row_display = "";
-
-  //   for (let y = 0; y < rowNumber; y = y + 1) {
-  //     row_display = `${row_display} ${singleNumber}`;
-  //     singleNumber = singleNumber + 1;
-  //   }
-
-  //   console.log(row_display);
-  // }
-
   return (
     <>
-      <div className="m-5 bg-pink-300 p-10 font-bold text-white">
-        PYRAMID SET
-        <input
-          type="number"
-          className=" border-black p-4 text-black"
-          value={rows}
-          onChange={(event) => {
-            set_rows(event.target.valueAsNumber);
-          }}
-        />
-      </div>
-      <div className="m-10 bg-rose-300 p-10 "></div>
-      {steps.map((step,index)=>{
-        <div key={step.toString()}>
-          <div className=" mx-auto flex p-2">
-            (step.bricks.map((brick)=> {
-              return(
-                <div key={brick.toString()}
-              )
-            }
+      <div className="bg-rose-300 p-10" />
+      <input
+        type="number"
+        value={rows}
+        onChange={(e) => set_rows(e.target.valueAsNumber)}
+      />
+
+      {steps.map((step, index) => (
+        <div key={step.toString()} className="m-1 flex gap-1 bg-pink-500">
+          <div className="mx-auto flex p-2">
+            {step.bricks.map((brick) => {
+              return (
+                <div key={brick.toString()} className="m-4 h-4 bg-red-300">
+                  {brick}
+                </div>
+              );
+            })}
           </div>
         </div>
-      }
-      )}
+      ))}
     </>
   );
 }
