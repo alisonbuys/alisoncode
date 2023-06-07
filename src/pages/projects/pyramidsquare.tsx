f7607845995b70bf8e9a4b1135549443af3cc69a
Alison Buys <alisonbuys15@gmail.com>
2023-06-07 14:52:07 +0200

    generate

---
+++ src/pages/projects/pyramidsquare.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+type Brick = {
+  id: string;
+  x: number;
+  y: number;
+};
+
+type PyramidStep = {
+  stepNumber: number;
+  bricks: Brick[];
+};
+
+export function generatePyramidStep(input: number): PyramidStep[] {
+  const pyramid: PyramidStep[] = [];
+  let counter = 0;
+
+  for (let y = 0; y < input; y++) {
+    const newStep: PyramidStep = { stepNumber: y, bricks: [] };
+    for (let x = 0; x < input; x++) {
+      let id = "test";
+      id = crypto.randomUUID();
+      newStep.bricks.push({ x, y, id }), counter++;
+    }
+    pyramid.push(newStep);
+  }
+
+  return pyramid;
+}
+
+export default function PyramidApp() {
+  const [rows, set_rows] = useState<number>(10);
+  const steps = generatePyramidStep(rows);
+
+  return (
+    <>
+      <input
+        className="ml-5 rounded-lg border-black bg-neutral-300 p-4"
+        type="number"
+        value={rows}
+        onChange={(e) => set_rows(e.target.valueAsNumber)}
+      />
+      {steps.map((step, index) => (
+        <div key={step.toString()} className="m-1 flex gap-1">
+          <div className=" mx-auto flex p-2">
+            {step.bricks.map((brick) => {
+              return (
+                <div
+                  key={brick.toString()}
+                  className=" m-0.5 rounded-md bg-white p-2"
+                >
+                  <pre>
+                    [{brick.x},{brick.y}]
+                  </pre>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
