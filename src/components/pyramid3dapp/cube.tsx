acc46ff7273db594f3ba9b680ec82d3efd15e713
Alison Buys <alisonbuys15@gmail.com>
2023-06-07 15:16:24 +0200

    styling

--- src/components/pyramid3dapp/cube.tsx
+++ src/components/pyramid3dapp/cube.tsx
@@ -1,9 +1,11 @@
 import { useThree, extend, type Vector3 } from "@react-three/fiber";
 import { useRef } from "react";
+import { generatePyramidStep } from "~/pages/projects/pyramidsquare";
 
 export default function Cube(props: { position: Vector3 }) {
   const { camera, gl } = useThree();
   const cubeRef = useRef<any>();
+  // const blocks = generatePyramidStep(rows);
 
   return (
     <>
