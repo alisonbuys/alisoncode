57a559a86297b7953a00e6492f89ee0f34e45cc1
Alison Buys <alisonbuys15@gmail.com>
2023-06-12 12:17:11 +0200

    add about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,22 @@
+import { FcInfo } from "react-icons/fc";
+
 export default function AboutPage() {
   return (
-    <div>
-      <h1>About</h1>
-      <p> Some about info goes here</p>
+    <div className=" mx-auto px-40">
+      <div className="flex flex-row gap-1 font-serif text-3xl">
+        <FcInfo />
+        Alison Buys
+      </div>
+      <div className="font-serif">
+        Outgoing and ambitious high school graduate.
+        <br />
+        Eager to use my programing skills to work as an software developer.
+        <br />
+        Problem solver eager to prove my value and talent.
+        <br />
+        Motivated to advance and expand my skill set through targeted mentorship
+        and challenging projects.
+      </div>
     </div>
   );
 }
