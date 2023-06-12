13b9141fd199f279490db0f28780bb3b7111b56b
Alison Buys <alisonbuys15@gmail.com>
2023-06-12 12:16:00 +0200

    add icons

--- src/components/applayout.tsx
+++ src/components/applayout.tsx
@@ -1,19 +1,46 @@
+import { HomeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { ReactNode } from "react";
+import {
+  FcHome,
+  FcAbout,
+  FcFolder,
+  FcPhone,
+  FcBusinessContact,
+} from "react-icons/fc";
 
 export function AppLayout(props: { children: ReactNode }) {
   return (
     <>
-      <div className="m-5 flex gap-4 rounded-lg bg-purple-200 p-5">
-        <Link href="/" className="hover:underline">
+      <div className="m-5 flex justify-end gap-4 rounded-lg bg-purple-200 p-5">
+        <Link
+          href="/"
+          className="flex flex-row items-center gap-2 rounded bg-purple-100 px-4 py-2 transition hover:bg-white hover:underline"
+        >
+          <FcHome />
           Home
         </Link>
 
-        <Link href="/about" className="hover:underline">
+        <Link
+          href="/about"
+          className="flex flex-row items-center gap-2 rounded bg-purple-100 px-4 py-2 transition hover:bg-white hover:underline"
+        >
+          <FcAbout />
           About
         </Link>
-        <Link href="/projects" className="hover:underline">
+        <Link
+          href="/projects"
+          className="flex flex-row items-center gap-2 rounded bg-purple-100 px-4 py-2 transition hover:bg-white hover:underline"
+        >
+          <FcFolder />
           Projects
+        </Link>
+        <Link
+          href="/contact"
+          className="flex flex-row items-center gap-2 rounded bg-purple-100 px-4 py-2 transition hover:bg-white hover:underline"
+        >
+          <FcBusinessContact />
+          Contact
         </Link>
       </div>
 
