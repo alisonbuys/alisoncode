488a99856cd6e46dca781453a9eb984677886342
Alison Buys <alisonbuys15@gmail.com>
2023-06-12 13:56:33 +0200

    add logo

--- src/components/applayout.tsx
+++ src/components/applayout.tsx
@@ -1,43 +1,61 @@
 import Link from "next/link";
 import { type ReactNode } from "react";
 import { FcHome, FcAbout, FcFolder, FcBusinessContact } from "react-icons/fc";
+import Image from "next/image";
 
 export function AppLayout(props: { children: ReactNode }) {
   return (
     <>
-      <div className="m-5 flex justify-end gap-4 rounded-lg bg-purple-200 p-5">
-        <Link
-          href="/"
-          className="flex flex-row items-center gap-2 rounded bg-purple-100 px-4 py-2 transition hover:bg-white hover:underline"
-        >
-          <FcHome />
-          Home
-        </Link>
+      <div className="container mx-auto mt-5">
+        <div className="flex gap-4 rounded-lg bg-purple-200 p-2">
+          <div className="flex justify-start">
+            <div className="flex h-20 justify-start gap-1 p-4">
+              <Image
+                height={100}
+                width={100}
+                src={"/assets/images/logo.png"}
+                className="h-14 w-14 rounded-full"
+                alt={"logo"}
+              />
+              <div className=" border-l border-purple-700 pl-2 pt-2 font-serif text-lg">
+                Web Developer
+              </div>
+            </div>
+          </div>
+          <Link
+            href="/"
+            className="flex flex-row items-center gap-2 rounded bg-purple-100 px-4 py-2 transition hover:bg-white hover:underline"
+          >
+            <FcHome />
+            Home
+          </Link>
 
-        <Link
-          href="/about"
-          className="flex flex-row items-center gap-2 rounded bg-purple-100 px-4 py-2 transition hover:bg-white hover:underline"
-        >
-          <FcAbout />
-          About
-        </Link>
-        <Link
-          href="/projects"
-          className="flex flex-row items-center gap-2 rounded bg-purple-100 px-4 py-2 transition hover:bg-white hover:underline"
-        >
-          <FcFolder />
-          Projects
-        </Link>
-        <Link
-          href="/contact"
-          className="flex flex-row items-center gap-2 rounded bg-purple-100 px-4 py-2 transition hover:bg-white hover:underline"
-        >
-          <FcBusinessContact />
-          Contact
-        </Link>
+          <Link
+            href="/about"
+            className="flex flex-row items-center gap-2 rounded bg-purple-100 px-4 py-2 transition hover:bg-white hover:underline"
+          >
+            <FcAbout />
+            About
+          </Link>
+          <Link
+            href="/projects"
+            className="flex flex-row items-center gap-2 rounded bg-purple-100 px-4 py-2 transition hover:bg-white hover:underline"
+          >
+            <FcFolder />
+            Projects
+          </Link>
+          <Link
+            href="/contact"
+            className="flex flex-row items-center gap-2 rounded bg-purple-100 px-4 py-2 transition hover:bg-white hover:underline"
+          >
+            <FcBusinessContact />
+            Contact
+          </Link>
+        </div>
       </div>
-
-      <div className="m-5 rounded-lg bg-neutral-100 p-5">{props.children}</div>
+      <div className="container m-5 mx-auto rounded-lg bg-neutral-100 p-5">
+        {props.children}
+      </div>
     </>
   );
 }
