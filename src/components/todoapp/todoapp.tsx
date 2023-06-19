97a577107ab55cc40e03f190df493c05545c0b46
Alison Buys <alisonbuys15@gmail.com>
2023-06-19 08:34:05 +0200

    update todoapp

--- src/components/todoapp/todoapp.tsx
+++ src/components/todoapp/todoapp.tsx
@@ -65,7 +65,7 @@ export function TodoApp() {
               key={onetodoitem.id}
               className={
                 onetodoitem.isDone === true
-                  ? "flex bg-rose-200 p-2 text-gray-500"
+                  ? "flex bg-rose-200 p-2 text-gray-500 line-through"
                   : "flex bg-rose-200 p-2 font-semibold"
               }
             >
