53eecd16b31232b4a5fcb73fd73899b32412f264
Alison Buys <alisonbuys15@gmail.com>
2023-06-19 13:29:08 +0200

    searchbar changes

--- src/components/designapp/searchbar.tsx
+++ src/components/designapp/searchbar.tsx
@@ -1,10 +1,12 @@
 import { type ReactNode } from "react";
 
-export default function SearchbarPage(props: { children: ReactNode }) {
+export default function Searchbar(props: { children: ReactNode }) {
   return (
     <>
       <div>
-        <div className="rounded-3xl bg-white p-2 pl-6 ">Search</div>
+        <div className="rounded-3xl bg-white p-2 pl-6 text-blue-200">
+          Search
+        </div>
       </div>
       <div className="">{props.children}</div>
     </>
