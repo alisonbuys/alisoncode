9ffd97a103746a4630c8aa831498cb53fc42cc5b
Alison Buys <alisonbuys15@gmail.com>
2023-06-26 14:41:09 +0200

    new page

---
+++ src/pages/profile.tsx
@@ -0,0 +1,11 @@
+// export { MessageApp } from "~/components/message";
+
+import { Profile } from "~/components/3dprofile/profile";
+
+export default function ProfilePage() {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+}
