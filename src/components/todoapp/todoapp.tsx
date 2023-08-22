233a9e01536f13fcda450323dd3876bb58ccc097
alisonbuys <alisonbuys15@gmail.com>
2023-08-22 14:47:44 +0200

    todo app chnages

--- src/components/todoapp/todoapp.tsx
+++ src/components/todoapp/todoapp.tsx
@@ -3,6 +3,10 @@ import { useEffect, useState } from "react";
 import { useTodoState } from "./todostate";
 import { FiTrash2 } from "react-icons/fi";
 import { BiSortDown, BiSortUp, BiTask } from "react-icons/bi";
+import Image from "next/image";
+import { FaSignal } from "react-icons/fa";
+import { MdOutlineWifi } from "react-icons/md";
+import { BsBatteryFull } from "react-icons/bs";
 
 export type TodoItem = {
   id: string;
@@ -26,13 +30,32 @@ export function TodoApp() {
   if (!loaded) return <div>loading...</div>;
 
   return (
-    <div className="rounded-xl  bg-rose-200 p-8 shadow-2xl">
-      <div className="p-2 text-lg font-normal text-white"> TODO LIST</div>
-      <div>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-slate-200 p-8">
+    <div className="container mx-auto border-2 border-black w-80 rounded-3xl bg-gradient-to-b from-rose-200 from-30% via-rose-300 via-70% to-yellow-200 to-90% ">
+       <div className="container p-1 px-3 flex flex-row bg bg-transparent text-white ">
+        <FaSignal className="h-4 w-4 mr-1"/>
+        <p className="text-xs font-bold ">CellC</p>
+        <MdOutlineWifi className=" ml-1 "/>
+        <p className="text-xs justify-center mx-auto font-bold">9:41
+        </p>
+        <div className="flex flex-row ">
+       <p className="text-xs font-bold">99%</p>
+        <BsBatteryFull className="ml-1 "/>
+        </div>
+      </div>
+      <div className="p-2 text-3xl font-bold text-white mx-auto text-center"> TODO APP</div>
+      <Image
+                height={100}
+                width={100}
+                src={"/assets/images/notepad.png"}
+                className=" mx-auto"
+                alt={"AI Picture of Alison"}
+              />
+      <div className="mx-1">
         <input
           type="text"
           placeholder="Add Todo..."
-          className="mb-5 border p-4"
+          className="mb-5 border p-3"
           value={newtodo}
           onChange={(e) => {
             newtodo_set(e.target.value);
@@ -52,7 +75,7 @@ export function TodoApp() {
 
             todostate.set({ todolist });
           }}
-          className="rounded-r-xl bg-rose-400 p-4 px-2 text-white transition hover:bg-rose-500"
+          className="rounded-r-xl bg-rose-400 p-3 px-2 text-white transition hover:bg-rose-500"
         >
           Add task
         </button>
@@ -65,8 +88,8 @@ export function TodoApp() {
               key={onetodoitem.id}
               className={
                 onetodoitem.isDone === true
-                  ? "flex bg-rose-200 p-2 text-gray-500 line-through"
-                  : "flex bg-rose-200 p-2 font-semibold"
+                  ? "flex p-2 text-gray-500 line-through mx-2"
+                  : "flex p-2 font-semibold mx-2"
               }
             >
               <span className="flex-1">
@@ -142,8 +165,20 @@ export function TodoApp() {
                 <FiTrash2 className="h-5 w-5"></FiTrash2>{" "}
               </button>
             </div>
+            
+          
           );
         })}
+         <div className="bg-black w-24 mx-auto mb-2 p-0.5 rounded-3xl my-40"></div>
     </div>
+   
+      
+    <p className="text-gray-500 p-5">Create a TODO list !
+   <br/> -Sort tasks
+   <br/> -Mark tasks as completed
+   <br/>-Delete tasks</p>
+    </main>
   );
 }
+
+
